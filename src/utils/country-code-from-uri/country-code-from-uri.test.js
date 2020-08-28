b0372b028a64c4cdb9753de646d81667c266ad3f
Rees <reesmorriscontact@gmail.com>
2020-08-28 13:07:00 +0100

    Add tests to 'countryCodeFromURI' function

--- src/utils/country-code-from-uri/country-code-from-uri.test.js
+++ src/utils/country-code-from-uri/country-code-from-uri.test.js
@@ -1,1 +1,19 @@
-test.todo('todo');
+import countryCodeFromURI from './country-code-from-uri';
+
+const formatCode = code => `urn:iso:std:3166:-2:${code}`;
+
+test('returns `null` if no URI is provided', () => {
+  expect(countryCodeFromURI()).toBe(null);
+});
+
+test('returns `null` if URI is not a string', () => {
+  expect(countryCodeFromURI({ abc: 'def' })).toBe(null);
+});
+
+test('returns correct country codes', () => {
+  // format: urn:iso:std:3166:-2:XX
+  expect(countryCodeFromURI(formatCode('CH'))).toBe('CH');
+  expect(countryCodeFromURI(formatCode('IN'))).toBe('IN');
+  expect(countryCodeFromURI(formatCode('UK'))).toBe('UK');
+  expect(countryCodeFromURI(formatCode('SA'))).toBe('SA');
+});

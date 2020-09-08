970ecbb86ad823f66f797370eeea3ddb1869627b
Rees <reesmorriscontact@gmail.com>
2020-09-08 15:17:53 +0100

    Fix issue with timestamp test

--- src/utils/format-timestamp/format-timestamp.test.js
+++ src/utils/format-timestamp/format-timestamp.test.js
@@ -3,7 +3,7 @@ import formatTimestamp from './format-timestamp';
 test('returns a timestamp formatted correctly', () => {
   const timestamps = [
     { timestamp: '2020-09-08T14:04:39.445Z', expected: 'Sep 2020' },
-    { timestamp: '1998-07-31T23:00:00.000Z', expected: 'Aug 1998' },
+    { timestamp: '1998-07-29T23:00:00.000Z', expected: 'Jul 1998' },
     { timestamp: '2012-05-29T23:00:00.000Z', expected: 'May 2012' },
     { timestamp: '2050-01-01T00:00:00.000Z', expected: 'Jan 2050' }
   ];

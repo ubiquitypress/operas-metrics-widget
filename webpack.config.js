d8e3f447ea8c2b21fb069ae09cb7d8ebd97dc77a
Rees <reesmorriscontact@gmail.com>
2021-05-07 12:02:29 +0100

    Reconfigure Webpack to separate CSS files from JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   entry: './src/index.jsx',
   output: {
@@ -19,9 +21,34 @@ module.exports = {
         }
       },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Use `style-loader` to render things during development, but we want to separate
+          // the CSS from the JS when building, so we'll use the MiniCssExtractPlugin for that
+          process.env.NODE_ENV === 'development'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+
+          // We're telling `css-loader` to rename our classes `mw__[local]` so they can be
+          // overriden by anyone using the widget - just be careful of conflicts.
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: 'mw__[local]'
+              }
+            }
+          },
+
+          // Using `sass-loader` allows us to use .scss files, making things much cleaner
+          'sass-loader'
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'widget.css'
+    })
+  ]
 };

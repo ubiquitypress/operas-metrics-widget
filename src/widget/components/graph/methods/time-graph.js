989b8b31b9395731f253576715a8ef36a5741386
Rees <reesmorriscontact@gmail.com>
2021-05-12 16:48:43 +0100

    Overhaul the timeGraph() method to provide much better results

--- src/widget/components/graph/methods/time-graph.js
+++ src/widget/components/graph/methods/time-graph.js
@@ -1,100 +1,91 @@
+import dayjs from 'dayjs';
 import formatTimestamp from '../../../utils/format-timestamp';
 
-// TODO: Can we clean this up?
-const timeGraph = ({ t, uris, tab }) => {
-  // Make a key/value dictionary for each date
+const timeGraph = ({ t, tab, uris }) => {
+  // Merge URIs into a single object
   const dates = {};
-  uris.forEach(item => {
-    const date = item.timestamp;
-    if (date) dates[date] = dates[date] ? dates[date] + item.value : item.value;
+  uris.forEach(({ timestamp, value }) => {
+    if (timestamp && value)
+      dates[timestamp] = dates[timestamp] ? dates[timestamp] + value : value;
   });
 
-  // There is no data
-  if (Object.keys(dates).length === 0) return {};
-
-  // Dates should already be in order, but sort them just to be sure.
-  // This function will sort by the oldest dates first
-  const sorted = [];
-  Object.keys(dates).forEach(date => {
-    sorted.push({ key: date, value: dates[date] });
+  // Sort the data into an array to find the earliest entry
+  let data = [];
+  Object.entries(dates).forEach(([key, value]) => {
+    data.push({ key, value });
   });
-  sorted.sort((a, b) => {
+  data.sort((a, b) => {
     if (a.key < b.key) return -1;
     if (a.key > b.key) return 1;
     return 0;
   });
 
-  // Make sure we have no missing data for a month
-  const allDates = [];
-  const uniqueDates = []; // used for calculating the midpoint
-  try {
-    const [todayYear, todayMonth] = new Date().toISOString().split('-');
-    let [year, month] = sorted[0].key.split('-');
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      // eslint-disable-next-line no-loop-func
-      const match = sorted.filter(({ key }) =>
-        key.includes(`${year}-${month}`)
-      );
-      if (match.length > 0) match.forEach(item => allDates.push(item));
-      else
-        allDates.push({
-          key: `${year}-${month}-01T00:00:00+0000`,
-          value: 0
-        });
-      uniqueDates.push(`${year}-${month}-01T00:00:00+0000`);
-
-      // Get the next date
-      let nextMonth = (Number.parseInt(month, 10) + 1).toString();
-      if (nextMonth === '13') nextMonth = '01';
-      if (nextMonth.length === 1) nextMonth = `0${nextMonth}`;
-      month = nextMonth;
-      year =
-        nextMonth === '01' ? (Number.parseInt(year, 10) + 1).toString() : year;
-
-      if (year >= todayYear && month > todayMonth) break;
+  if (data.length > 0) {
+    // If there is only one item, add a zero value for the previous month
+    if (data.length === 1) {
+      const prevMonth = dayjs(new Date(data[0].key))
+        .subtract(1, 'month')
+        .toISOString();
+      data.push({ key: prevMonth, value: 0 });
     }
-  } catch (err) {
-    // Something went wrong
-    // eslint-disable-next-line no-console
-    console.error(err);
-    sorted.forEach(item => allDates.push(item));
+
+    // Determine the number of months between the first metric and today
+    const initialDate = new Date(data[0].key);
+    const monthsDiff = dayjs(new Date()).diff(initialDate, 'month', true);
+    const countDays = monthsDiff <= 0.25; // show days instead of months if data is only 1 week old
+
+    // Make a dictionary of each month between
+    const months = {};
+    if (!countDays) {
+      for (let i = 0; i <= Math.floor(monthsDiff); i += 1) {
+        const [year, month] = dayjs(initialDate)
+          .add(i, 'months')
+          .toISOString()
+          .split('-');
+        months[`${year}-${month}`] = 0;
+      }
+    }
+
+    // Accumulate the data into the months dictionary
+    data.forEach(({ key, value }) => {
+      const [year, month, day] = key.split('-');
+      const field = !countDays ? `${year}-${month}` : `${year}-${month}-${day}`;
+      months[field] = months[field] ? months[field] + value : value;
+    });
+
+    // Sort into an array again
+    data = [];
+    Object.entries(months).forEach(([key, value]) => {
+      data.push({ key, value });
+    });
+    data.sort((a, b) => {
+      if (a.key < b.key) return -1;
+      if (a.key > b.key) return 1;
+      return 0;
+    });
+
+    // Aggregate the data
+    data.forEach((item, index) => {
+      const aggregate =
+        index === 0 ? item.value : item.value + data[index - 1].value;
+      data[index] = {
+        ...data[index],
+        key: formatTimestamp(t, data[index].key, countDays ? 'long' : 'short'),
+        value: aggregate
+      };
+    });
   }
 
-  // Make sure we always have at least two data points
-  // since the graph will render as empty if there is only one
-  if (allDates.length === 1) {
-    const dayBefore = new Date(sorted[0].key);
-    dayBefore.setDate(dayBefore.getDate() - 1);
-    allDates.unshift({ key: dayBefore.toISOString(), value: 0 });
-  }
-
-  // Go through each item to make it cumulative
-  allDates.forEach((item, i) => {
-    if (i > 0) allDates[i].value += allDates[i - 1].value;
+  // Pull the axes
+  const seriesData = [];
+  const xAxisCategories = [];
+  data.forEach(({ key, value }) => {
+    seriesData.push(value);
+    xAxisCategories.push(key);
   });
 
-  // Determine the xAxis categories
-  const xAxis = Array(allDates.length)
-    .fill(null)
-    .map(() => '');
-  xAxis[0] = formatTimestamp(t, uniqueDates[0]); // first
-  xAxis[Math.floor(allDates.length / 2)] = // median
-    uniqueDates.length > 2
-      ? formatTimestamp(t, uniqueDates[Math.floor(uniqueDates.length / 2) - 1])
-      : '';
-  xAxis[allDates.length - 1] = formatTimestamp(
-    t,
-    uniqueDates[uniqueDates.length - 1]
-  ); // last
-
-  // Update the state with the new info
   return {
-    data: {
-      seriesData: allDates.map(date => date.value),
-      seriesName: t(`tabs.${tab}`),
-      xAxisCategories: xAxis
-    }
+    data: { seriesData, seriesName: t(`tabs.${tab}`), xAxisCategories }
   };
 };
 
